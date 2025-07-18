187ca31d1388c0bc52048a28fb21b1e1aaf970f7
Adarshkumar111 <akbishwas2883@gmail.com>
2025-07-18 20:07:15 +0530

    fix: dynamic port for render

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import express from 'express'
+import dotenv from "dotenv"
 import cors from 'cors'
 import databaseConnection from './database.js'
 import bookRouter from './routes/book.route.js'
 
+dotenv.config()
 databaseConnection()
 
-const app=express()
+const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Hello world")
 })
 
 app.use("/book", bookRouter)
 
-app.listen(8000,()=>{
-    console.log("Port listten on 8000")
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
